526d19880a9e225baacd15cd787de586f918065c
Vineeth <96077560+vineethevk@users.noreply.github.com>
2023-01-15 23:55:14 +0530

    mui sidebar

---
+++ Dashboard/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Dashboard from './components/Appbar'
+
+function App() {
+
+
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
